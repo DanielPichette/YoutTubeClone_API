2ff58bf1534d577d5fdb22d2eddd543863d65557
RayHubb <hubbard_raimon@yahoo.com>
2021-05-28 09:31:17 -0500

    Refined the comment model and separated the reply from the comment

--- comments/models.py
+++ comments/models.py
@@ -2,13 +2,19 @@ from django.db import models
 
 
 class Comment(models.Model):
-    video = models.CharField(max_length=50)
+    video_id = models.CharField(max_length=50)
     body = models.TextField(max_length=750)
     comment_likes = models.IntegerField(default=0)
     comment_dislikes = models.IntegerField(default=0)
-    reply = models.TextField(max_length=750)
 
-    def __str__(self):
-        return self.Comments.title
+    def __int__(self):
+        return self.video_id
+
+
+class Reply(models.Model):
+    comment = models.ForeignKey('Comment')
+    body = models.TextField(max_length=750)
+    comment_likes = models.IntegerField(default=0)
+    comment_dislikes = models.IntegerField(default=0)
 
 
--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from .models import Comment
 from .serializers import CommentSerializer
@@ -21,28 +22,33 @@ class AllComments(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class CommentDetail(APIView):
 
-    def get_object(self, pk):  # selecting a song object by the pk id
+class CommentDetail(APIView):
+    def get_object(self, pk):  # selecting a comment object by the pk id
         try:
-                return Comment.objects.get(pk=pk)  # here we returning the song by the id
-        except Comment.DoesNotExist:  # if the exception 'song.does.not.exist' occurs, ...
+            return Comment.objects.get(pk=pk)  # here we returning the comment by the id
+        except Comment.DoesNotExist:  # if the exception 'comment.does.not.exist' occurs, ...
             raise Http404  # then we respond with an HTTP404 response
         # TODO : check on what raise Http404 does exactly. get clarification
 
     def get(self, request, pk):  # 'get' is getting something fom the database.. in this case a single song
         comment = self.get_object(pk)  # here we are selecting a song by id, and assigning it to the 'song' variable
         serializer = CommentSerializer(comment)  # here we are serializing/reformatting the song variable
-        return Response(serializer.data, status=status.HTTP_200_OK)  # here we are returning the selected song, in the serialized/updated format
+        return Response(serializer.data,
+                        status=status.HTTP_200_OK)  # here we are returning the selected song, in the serialized/updated format
+
     # get song by id functionality
 
     def put(self, request, pk):  # 'put' syntax == update functionality
         comment = self.get_object(pk)  # here we are getting the song by id using the get_object function
-        serializer = CommentSerializer(comment, data=request.data)  # here we are reformatting the song using the serializer
+        serializer = CommentSerializer(comment,
+                                       data=request.data)  # here we are reformatting the song using the serializer
         if serializer.is_valid():  # == if the data in the request matches the model ...
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)  # save the selected song with the new data from the request
+            return Response(serializer.data,
+                            status=status.HTTP_200_OK)  # save the selected song with the new data from the request
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # otherwise give 404 error message
+
     # update song by id functionality
 
     def delete(self, request, pk):  # delete functionality
